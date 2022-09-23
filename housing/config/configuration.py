8ad61052020f3d8711a06f83d070beb450633b79
Vinayak <manevinayakv.47@gmail.com>
2022-09-23 16:37:22 +0530

    updates

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -14,13 +14,16 @@ from housing.constant import *
 
 class Configuration : 
 
-    def __init__(self , 
-        config_file_path = CONFIG_FILE_PATH , 
-        current_time_stamp:str= CURRENT_TIME_STAMP) -> None:
-
-        self.config_info = read_yaml_file(file_path=config_file_path) 
-        self.training_pipeline_config = self.get_training_pipeline_config()
-        self.time_stamp = current_time_stamp 
+    def __init__(self,
+        config_file_path:str =CONFIG_FILE_PATH,
+        current_time_stamp:str = CURRENT_TIME_STAMP
+        ) -> None:
+        try:
+            self.config_info  = read_yaml_file(file_path=config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            raise HousingException(e,sys) from e 
 
 
 
